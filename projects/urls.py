63999a280e17abff092d93576e2fdde2897c0f91
C0mplex <gaybullayevodiljon@gmail.com>
2023-04-03 18:02:13 +0500

    work

--- projects/urls.py
+++ projects/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import base, projects, project_item, instagram_downloader_, youtube_downloader_, coins, C0mplexTranslate
+from .views import base, projects, project_item, instagram_downloader_, youtube_downloader_, coins, C0mplexTranslate, ImageCompare, avtotest
 
 urlpatterns=[
     path('', base, name='base'),
@@ -8,6 +8,8 @@ urlpatterns=[
     path('youtube_downloader/', youtube_downloader_, name='youtube_downloader'),
     path('coins/', coins, name='coins'),
     path('translate/', C0mplexTranslate, name='translate'),
+    path('imagecompare/', ImageCompare, name='imagecompare'),
+    path('avtotest/', avtotest, name='avtotest'),
     path('projects/', projects, name='projects'),
     path('project/<int:id>/', project_item, name='item')
 ]
