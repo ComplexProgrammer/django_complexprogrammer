99d8935ad43ba3f05ef6b8adc46d609db8b9a905
C0mplex <gaybullayevodiljon@gmail.com>
2023-04-14 16:20:45 +0500

    w

--- django_complexprogrammer/settings.py
+++ django_complexprogrammer/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@7#81#)@a)etb_pd3gd#-$z6l4z8f)u=wp0@6z-zz2iza7t8nc'
+SECRET_KEY = config('SECRET_KEY', default=False, cast=bool)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
