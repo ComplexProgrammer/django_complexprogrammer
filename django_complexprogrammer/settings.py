e4e4ef2aebcc0795e2c3fc511d3e4276e7a17af3
C0mplex <gaybullayevodiljon@gmail.com>
2023-04-19 11:39:44 +0500

    w

--- django_complexprogrammer/settings.py
+++ django_complexprogrammer/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 # ALLOWED_HOSTS =  ['*']
-ALLOWED_HOSTS =  ['complexprogrammer-dev.uz', 'www.complexprogrammer-dev.uz', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS =  ['complexprogrammer.uz', 'www.complexprogrammer.uz', 'complexprogrammer-dev.uz', 'www.complexprogrammer-dev.uz', '127.0.0.1', 'localhost']
 
 
 # Application definition
