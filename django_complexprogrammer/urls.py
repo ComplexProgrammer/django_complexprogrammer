51c0a0e5c767295f0ef459d5a945a5ba5c2041bb
C0mplex <gaybullayevodiljon@gmail.com>
2023-04-19 10:37:02 +0500

    w

--- django_complexprogrammer/urls.py
+++ django_complexprogrammer/urls.py
@@ -23,9 +23,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('projects.urls'))
 ]
-urlpatterns +=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }), ]
-# if settings.DEBUG:
-#     urlpatterns +=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns +=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     
