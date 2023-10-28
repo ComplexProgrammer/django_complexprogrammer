8a0dc8b4bc979f20163f42280f274c99251ae39d
C0mplex <gaybullayevodiljon@gmail.com>
2023-10-28 18:28:05 +0500

    1

--- django_complexprogrammer/urls.py
+++ django_complexprogrammer/urls.py
@@ -19,7 +19,6 @@ from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.static import serve
 from django.conf.urls.i18n import i18n_patterns
-from django.views.i18n import JavaScriptCatalog
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('projects.urls'))
@@ -29,9 +28,6 @@ if settings.DEBUG:
     urlpatterns +=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += [path('i18n/', include('django.conf.urls.i18n')),]
-urlpatterns = i18n_patterns(
-    path("jsi18n/", JavaScriptCatalog.as_view(), name="javascript-catalog"),
-)
 urlpatterns = [
-    *i18n_patterns(*urlpatterns, prefix_default_language=False),
+    *i18n_patterns(*urlpatterns, prefix_default_language=True),
     ]
