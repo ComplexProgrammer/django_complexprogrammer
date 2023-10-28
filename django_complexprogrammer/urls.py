f5c3ec40a4b69d422a289f061370764df5d9ac66
C0mplex <gaybullayevodiljon@gmail.com>
2023-10-28 18:40:58 +0500

    1

--- django_complexprogrammer/urls.py
+++ django_complexprogrammer/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.static import serve
 from django.conf.urls.i18n import i18n_patterns
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('projects.urls'))
