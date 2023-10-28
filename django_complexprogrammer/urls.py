c3088b3784fb02cf50ffea7cd6f6f1afa3964a70
C0mplex <gaybullayevodiljon@gmail.com>
2023-10-28 18:29:28 +0500

    1

--- django_complexprogrammer/urls.py
+++ django_complexprogrammer/urls.py
@@ -29,5 +29,5 @@ if settings.DEBUG:
     # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += [path('i18n/', include('django.conf.urls.i18n')),]
 urlpatterns = [
-    *i18n_patterns(*urlpatterns, prefix_default_language=True),
+    *i18n_patterns(*urlpatterns, prefix_default_language=False),
     ]
