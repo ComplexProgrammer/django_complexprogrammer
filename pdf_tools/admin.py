161a5a55b97a570d90bad2f7a7d7fae70d18f886
C0mplex <gaybullayevodiljon@gmail.com>
2023-11-06 16:29:05 +0500

    w

--- pdf_tools/admin.py
+++ pdf_tools/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from pdf_tools.models import File
+
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
+    list_display=['existingPath', 'name', 'eof']
+    list_filter=['existingPath', 'name', 'eof']
+    search_fields=['existingPath', 'name', 'eof']
+    ordering=['id']
