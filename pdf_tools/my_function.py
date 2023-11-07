ac399cffc1381c2e577ab25a56045911c9c6f5ae
C0mplex <gaybullayevodiljon@gmail.com>
2023-11-07 19:34:58 +0500

    w

--- pdf_tools/my_function.py
+++ pdf_tools/my_function.py
@@ -1,4 +1,7 @@
 from pdf2docx import parse
+from pdf2image.pdf2image import convert_from_path
+import tabula
+from webpage2pdf import RenderManager
 from typing import Tuple
 
 def convert_pdf2docs(input_file :str, pages: Tuple = None): # type: ignore
@@ -16,6 +19,32 @@ def convert_pdf2docs(input_file :str, pages: Tuple = None): # type: ignore
     print("\n".join("{}:{}".format(i, j) for i , j in summary.items()))
     print("#####################################################################")
     return summary
+
+# def convert_p2excel(input_file):
+#     df = read_pdf(input_file, pages='all')[0]
+#     convert_into(input_file, input_file.replace(".pdf",".csv"), output_format="csv", pages='all')
+#     print(df)
+#     return input_file.replace(".pdf",".csv")
+
+def convert_pdf2image(input_file):
+    # images = convert_from_path(input_file, poppler_path=r'C:\poppler-23.11.0\Library\bin')
+    pages = convert_from_path(input_file, 500, poppler_path=r'C:\poppler-23.11.0\Library\bin')
+    i=0
+    for page in pages:                    
+        i=i+1
+        page.save(i.__str__() +'-'+input_file.lower().replace(".pdf",".jpg"),'JPEG')
+    return '1-'+input_file.lower().replace(".pdf",".jpg")
+
+def url_to_pdf(url):
+    rm=RenderManager()
+    rm.addRender(num=2,showUI=True)
+    rm.from_url(url,'0.pdf')
+    rm.from_html("Hello World!",'1.pdf')
+    #rm.from_localFile('html/test.html','2.pdf')
+    print('start.')
+    rm.waitFinish()
+    print('finish all.')		
+    return '0.pdf'
 
 def handle_uploaded_file(f):  
     with open('static/upload/'+f.name, 'wb+') as destination:  
--- pdf_tools/views.py
+++ pdf_tools/views.py
@@ -2,21 +2,20 @@ from django.shortcuts import render
 from django.http import FileResponse, HttpResponse, JsonResponse
 from pdf_tools.forms import PdfForm
 from pdf_tools.models import File
-from pdf_tools.my_function import convert_pdf2docs, handle_uploaded_file
+from pdf_tools.my_function import convert_pdf2docs, convert_pdf2image, handle_uploaded_file
 from projects.views import send_file_
 
 def index(request):
     if request.method == 'POST':  
+        type= request.POST['type']
         file = request.FILES['file'].read()
         fileName= request.POST['filename']
         end = request.POST['end']
-        nextSlice = request.POST['nextSlice']
-        
-        if file=="" or fileName=="" or end=="" or nextSlice=="":
+        path = '' + fileName
+        if file=="" or fileName=="" or end=="":
             res = JsonResponse({'data':'Invalid Request'})
             return res
         else:
-            path = '' + fileName
             with open(path, 'wb+') as destination: 
                 destination.write(file)
             FileFolder = File()
@@ -24,10 +23,23 @@ def index(request):
             FileFolder.eof = end
             FileFolder.name = fileName
             FileFolder.save()
-            result = convert_pdf2docs(path)
-            print(result['Output File'])
-            res = JsonResponse({'data':'Uploaded Successfully','existingPath': fileName, 'result': result['Output File']})
-            return res
+            if type=='pdf2docx':
+                result = convert_pdf2docs(path)
+                print(result['Output File'])
+                res = JsonResponse({'data':'Uploaded Successfully','existingPath': fileName, 'result': result['Output File']})
+                return res
+            if type == 'pdf2excel':
+                result=convert_p2excel(path)
+                print(result)
+                res = JsonResponse({'data':'Uploaded Successfully','existingPath': fileName, 'result': result})
+                return res
+            if type == 'pdf2image':
+                result = convert_pdf2image(path)
+                print(result)
+                res = JsonResponse({'data':'Uploaded Successfully','existingPath': fileName, 'result': result})
+                return res
+
+
     else:
         return render(request, "pdf_tools/index.html")
     # input_file = "pc.pdf"
