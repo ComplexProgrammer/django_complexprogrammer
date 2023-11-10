2fe8cb0c5242898f2d476f98c1b4cdf43b888c82
C0mplex <gaybullayevodiljon@gmail.com>
2023-11-11 00:18:12 +0500

    w

--- tests/views.py
+++ tests/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.template import loader
@@ -8,6 +9,7 @@ def tests(request):
     data=[]
     type='group'
     type_id=0
+    type_data=[]
     group_id = request.GET.get('group_id', False)
     book_id = request.GET.get('book_id', False)
     topic_id = request.GET.get('topic_id', False)
@@ -18,22 +20,27 @@ def tests(request):
         if group_id is not False:
             type='book'
             type_id=group_id
+            type_data=Groups.objects.filter(id=group_id).values().first()
             data=Books.objects.filter(group_id=group_id).values()
         if book_id is not False:
             type='topic'
             type_id=book_id
+            type_data=Books.objects.filter(id=book_id).values().first()
             data=Topics.objects.filter(book_id=book_id).values()
         if topic_id is not False:
             type='question'
             type_id=topic_id
+            type_data=Topics.objects.filter(id=topic_id).values().first()
             data=Questions.objects.filter(topic_id=topic_id).values()
         if question_id is not False:
             type='answer'
             type_id=question_id
-            data=Answers.objects.filter(question_id=question_id).values()
+            type_data=Questions.objects.filter(id=question_id).values().first()
+            data=Answers.objects.filter(question_id=question_id).values() # type: ignore
     context={
         'data': data,
         'type': type,
+        'type_data':type_data,
         'type_id':type_id
     }
     return render(request, 'tests/index.html', context=context)
