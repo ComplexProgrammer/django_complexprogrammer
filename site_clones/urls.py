c8b4f2186480a202fefd46d6ec95b712ead76e52
C0mplex <gaybullayevodiljon@gmail.com>
2024-01-24 15:41:44 +0500

    w

--- site_clones/urls.py
+++ site_clones/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('site_clones/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('uzinfobiz.ru/', views.uzinfobiz_ru, name='uzinfobiz.ru'),
     path('uzinterbiz.com/', views.uzinterbiz_com, name='uzinterbiz.com'),
     path('postda.uz/', views.postda_uz, name='postda.uz'),
