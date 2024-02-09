f8809821601ea55b14dd6a3fb95c9374a728cbeb
C0mplex <gaybullayevodiljon@gmail.com>
2024-02-09 14:36:30 +0500

    w

--- django_complexprogrammer/urls.py
+++ django_complexprogrammer/urls.py
@@ -19,7 +19,6 @@ from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.static import serve
 from django.conf.urls.i18n import i18n_patterns
-from tests import views
 from django.views.generic import RedirectView
 # urlpatterns = [
 #     path('admin/', admin.site.urls),
