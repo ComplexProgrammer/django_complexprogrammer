0c848ae44474407ba9edfb9b46a3fb57abcc5edb
C0mplex <gaybullayevodiljon@gmail.com>
2024-02-10 12:06:12 +0500

    w

--- blog/views.py
+++ blog/views.py
@@ -3,15 +3,15 @@ from django.shortcuts import render
 from blog.models import Categories, Posts
 def index(request):
     id = request.GET.get('id', 0)
-    data=[]
+    post=[]
+    posts=Posts.objects.filter(is_deleted=False).values()
     categories=Categories.objects.filter(is_deleted=False).values()
-    if id == 0:
-        data=Posts.objects.filter(is_deleted=False).values()
-    else:
-        data=Posts.objects.filter(id=id).values().first()
+    if id != 0:
+        post=Posts.objects.filter(id=id).values().first()
     context={
         'id':id,
-        'data': data,
-        'categories':categories
+        'post': post,
+        'posts':posts,
+        'categories':categories,
     }
     return render(request, 'blog/index.html', context=context)
