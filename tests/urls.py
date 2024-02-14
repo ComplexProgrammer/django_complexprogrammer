a8df36ba02c6104b2293ae2c25b1dcea912bcf46
C0mplex <gaybullayevodiljon@gmail.com>
2024-02-14 16:49:20 +0500

    w

--- tests/urls.py
+++ tests/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('tests/', views.tests, name='tests'),
+    path('', views.tests, name='tests'),
     path('GetGroups/', views.GetGroups, name='GetGroups'), # type: ignore
     path('GetBooks/', views.GetBooks, name='GetBooks'), # type: ignore
     path('GetTopics/', views.GetTopics, name='GetTopics'), # type: ignore
