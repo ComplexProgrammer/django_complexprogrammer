b5c127cb5695d7e0d65118d25e932d53a0e4f671
C0mplex <gaybullayevodiljon@gmail.com>
2024-02-18 12:07:54 +0500

    w

--- test.py
+++ test.py
@@ -1,8 +1,19 @@
-import sys
-def main(a):
-    if a[1:][0]=='0':
-        print("Skript tugadi")
-        sys.exit()
-    print("Skript boshlandi")
-if __name__ == "__main__":
-    main(sys.argv)
+import subprocess
+
+# Kutubxonalar ro'yxatini olish
+installed_packages = subprocess.check_output(['pip', 'list']).decode('utf-8').split('\n')[2:]
+print(installed_packages)
+# Har bir kutubxonani hajmini aniqlash
+package_sizes = {}
+for package in installed_packages:
+    if package.strip():
+        parts = package.split()
+        package_name = parts[0]
+        package_size = parts[-1]
+        package_sizes[package_name] = package_size
+        print(package_name, package_size)
+
+# Eng katta hajmni aniqlash
+# largest_package = max(package_sizes.items(), key=lambda x: int(x[1]))
+
+# print(f"Eng katta hajmli kutubxona: {largest_package[0]} - {largest_package[1]}")
